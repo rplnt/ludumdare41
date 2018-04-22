8a196d8e06ae3fcad0eb5245a40d06f2273c7086
Matej Buday <rplnt@o0.sk>
2018-04-22 08:41:19 +0200

    Day 1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    TileManager tm;
+
+    public GameObject placer;
+    public bool placing;
+    Vector3 placingPos;
+
+
+    void Start () {
+        placing = false;
+        tm = this.GetComponent<TileManager>();
+        placer.SetActive(false);
+	}
+
+
+    void Update () {
+        placer.SetActive(placing);
+
+        if (placing) {
+            placer.transform.position = tm.HoveringTilePos(placingPos);
+        }
+	}
+
+
+
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+    public GameObject map;
+    Tilemap level;
+
+    public Vector2Int limitsLow, limitsHigh;
+
+    public Tile grass;
+    public Tile[] towers;
+    public Color[] colors;
+    int c = 0;
+
+
+    void Start () {
+        level = map.transform.Find("Base").GetComponent<Tilemap>();
+    }
+	
+
+    void Update () {
+        
+        if (Input.GetMouseButtonDown(0)) {
+
+            PlaceTowerAtMousePosition(0, colors[c % colors.Length]);
+            c++;
+
+        }
+
+    }
+
+
+    public Vector3 HoveringTilePos(Vector3 currentPos) {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int tilePos = level.WorldToCell(pos);
+
+        if (tilePos.x < limitsLow.x || tilePos.x > limitsHigh.x || tilePos.y < limitsLow.y || tilePos.y > limitsHigh.y) {
+            return currentPos;
+        }
+
+        return level.GetCellCenterWorld(tilePos);
+    }
+
+
+    public Vector3 PlaceTowerAt(int upgrade, Color color, Vector3Int pos) {
+        level.SetTile(pos, towers[upgrade]);
+
+        return level.GetCellCenterWorld(pos);
+    }
+
+    public Vector3 PlaceTowerAtMousePosition(int upgrade, Color color) {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int tilePos = level.WorldToCell(pos);
+
+        return PlaceTowerAt(upgrade, color, tilePos);
+    }
+}
